2a9243106bfc1d28a73c3005bdfff0191b1bb4c1
ashok770 <ashoktamata6002@gmail.com>
2026-04-16 12:26:01 +0530

    adding the api/logs

--- content.js
+++ content.js
@@ -4,7 +4,7 @@ setTimeout(() => {
   let found = false;
 
   elements.forEach((el) => {
-    const text = el.innerText.toLowerCase();
+    const text = el.innerText?.toLowerCase();
 
     if (!text || text.length < 20) return;
 
@@ -18,37 +18,6 @@ setTimeout(() => {
     ) {
       found = true;
 
-      // 🔥 Highlight element
-      el.style.border = "3px solid red";
-      el.style.backgroundColor = "#ffe6e6";
-    }
-  });
-
-  if (found) {
-    alert("⚠️ Dark Pattern Detected & Highlighted!");
-  }
-}, 3000);
-setTimeout(() => {
-  const elements = document.querySelectorAll("p, span, button, a, li");
-
-  let found = false;
-
-  elements.forEach((el) => {
-    const text = el.innerText.toLowerCase();
-
-    if (!text || text.length < 20) return;
-
-    if (
-      (text.includes("only") && text.includes("left")) ||
-      text.includes("limited time") ||
-      text.includes("offer ends") ||
-      text.includes("hurry") ||
-      text.includes("in stock") ||
-      text.includes("people are viewing")
-    ) {
-      found = true;
-
-      // 🔥 Highlight element
       el.style.border = "3px solid red";
       el.style.backgroundColor = "#ffe6e6";
     }
